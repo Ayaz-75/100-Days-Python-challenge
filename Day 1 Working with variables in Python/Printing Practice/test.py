684aa69daa79215164702e0afeda6bc8c4d840f0
Ayaz Lakho <82840513+Ayaz-75@users.noreply.github.com>
2025-01-03 19:28:07 +0500

    Python_Day_1

---
+++ Day 1 Working with variables in Python/Printing Practice/test.py
@@ -0,0 +1,55 @@
+# Fix the code below 👇
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \ and the letter n")
+
+
+
+# print('Hello' + " " + 'World!')
+# print("Hello", "World!, 123)")
+# # print("Hello!")
+
+
+# """
+# 1. 'eat'
+# 2. 'sleep'
+# 3. 'code'
+# 4. 'repeat'
+
+# """
+
+# print("1. 'eat'\n2. 'sleep'\n3. 'code'\n4. 'repeat'")
+
+
+
+# """
+# Printing Practice
+# Write a program that uses print statements to print the following recipe into the Output 
+# console. The text to print is already there, you just need to make it into code. 
+# Your code should print all five lines exactly the same as the example output below. 
+# Make sure that you don't change any of these existing text as everything, punctuation 
+# and casing all need to match!
+
+# """
+
+# print('Hello world!')
+
+# print('Hello world! \nHere is the first python recipe')
+
+
+# # print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+
+# # print("2. Knead the dough for 10 minutes.")
+
+# # print("3. Add 3g of Salt.")
+
+# # print("4. Leave to rise for 2 hours.")
+
+# # print("5. Bake at 200 degrees C for 30 minutes.")
+
+
+# # String concatination 
+# print("Hello" + " World")
---
+++ Day 1 Working with variables in Python/final_project_day_1.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator")
+user_city = input("Enter the name of city you grew up in: \n")
+pet_name = input("Enter you pet name: \n")
+print("You brand name could be ", user_city + " " + pet_name)
+
